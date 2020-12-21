e764312be7792fb82c07566c88af245aa4bc4178
ReginaWangui <reginahwangui801@gmail.com>
2020-12-21 10:03:35 +0300

    style

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 //Get the button
-var mybutton = document.getElementById("myBtn");
+var img2 = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+   img.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    img2.style.display = "none";
   }
 }
 

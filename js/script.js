2976d75aa35d64090ed974f77100203455c1aaf9
ReginaWangui <reginahwangui801@gmail.com>
2020-12-29 11:59:01 +0300

    added function

--- js/script.js
+++ js/script.js
@@ -1,27 +1,3 @@
-// const modal = document.querySelector(".modal");
-// const previews = document.querySelectorAll(".gallery img");
-// const original = document.querySelector(".full-img");
-// const imgText = document.querySelector(".caption");
-
-// previews.forEach((preview)  => { 
-//   previews.addEventListener("click",() =>{
-// modal.classList.add("open");
-// original.classList.add("open");
-//Dynamic change text and image
-// const originalsrc = preview.getAttribute("data-original");
-// originalsrc = './full/${originalSrc}';
-// const altText = previews.alt;
-// HTMLTableCaptionElement.textContent = altText;
-//   });
-// });
-//   modal.addEventListener("click" , (e) =>{
-//   if(e.target.classList.contains("modal")){
-//       modal.classList.remove("open");
-//       original.classList.remove("open");
-//     }
-
-// });
-
 $(document).ready(function(){
   $(".img1").click(function(){
     $(".design").toggle();
@@ -38,6 +14,10 @@ $(document).ready(function(){
     $(".text-click3").toggle();
   })
 });
-
+$(document).ready(function(){
+  $("#flip").click(function(){
+    $("#panel").slideDown("slow");
+  });
+});
 
  
